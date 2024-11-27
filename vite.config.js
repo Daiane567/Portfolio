76a28481032752ee3cf43e54bb3961c1c9055f30
Daiane Rocha <dr465796@gmail.com>
2024-11-27 01:24:46 +0100

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Use './' se o projeto não estiver na raiz
+  build: {
+    outDir: 'dist',
+  },
+  
 })
